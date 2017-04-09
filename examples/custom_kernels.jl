34f67608b245eb2c2f73476a58591f257fb52ec2
SimonDanisch <sdanisch@gmail.com>
2017-04-09 14:36:44 +0200

    use gpu_call

--- examples/custom_kernels.jl
+++ examples/custom_kernels.jl
@@ -12,9 +12,8 @@ end
 # want to execute the kernel! This kind of scheme might change in the future
 x = GPUArray(rand(Float32, 100))
 y = GPUArray(rand(Float32, 100))
-func = CLFunction(x, clmap!, sin, x, y)
 # same here, x is just passed to supply a kernel size!
-func(x, sin, x, y)
+gpu_call(x, clmap!, (sin, x, y))
 map!(sin, Array(y)) ≈ Array(x)
 
 # you can also use a kernel source string, directly with OpenCL code.
@@ -52,9 +51,7 @@ o_buff = similar(q_buff, UInt16)
 
 # you need to pass the name of the kernel you'd like to compile
 # It will as well need the "guiding array"
-jlfunc = CLFunction(o_buff, (source, :julia), q_buff, o_buff, UInt16(200))
-
-jlfunc(o_buff, q_buff, o_buff, UInt16(200))
+gpu_call(o_buff, (source, :julia), (q_buff, o_buff, UInt16(200)))
 # save out image!
 using FileIO, Colors
 x = Array(o_buff)
@@ -68,22 +65,19 @@ using GPUArrays
 using GPUArrays.CUBackend
 using CUDAnative
 CUBackend.init()
-function clmap!(f, out, b)
-    i = linear_index(out) # get the kernel index it gets scheduled on
-    out[i] = f(b[i])
-    return
-end
+
 # CUDA works exactly the same!
 x = GPUArray(rand(Float32, 100))
 y = GPUArray(rand(Float32, 100))
 # CUFunction instead of CLFunction
 # Note, that we need to use the sin of CUDAnative.
 # This necessity will hopefully be removed soon
-func = CUFunction(x, clmap!, CUDAnative.sin, x, y)
 # same here, x is just passed to supply a kernel size!
-func(x,  CUDAnative.sin, x, y)
+gpu_call(x, clmap!, (CUDAnative.sin, x, y))
+
 yjl = Array(y)
 map!(sin, yjl, yjl) ≈ Array(x)
+
 source = """
 __global__ void copy(const float *input, float *output)
 {
@@ -91,6 +85,5 @@ __global__ void copy(const float *input, float *output)
     output[i] = input[i];
 }
 """
-cucopy = CUFunction(x, (source, :copy), x, y)
-cucopy(x, x, y)
+gpu_call(x, (source, :copy), (x, y))
 Array(x) == Array(y)
