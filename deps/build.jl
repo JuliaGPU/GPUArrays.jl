0bdb44aeb8db02f9c9124ac6c7676474e89dc885
SimonDanisch <sdanisch@gmail.com>
2017-06-06 12:51:35 +0200

    add back opengl backend

--- deps/build.jl
+++ deps/build.jl
@@ -72,19 +72,19 @@ end
 
 # TODO add back OpenGL backend.. Currently not supported due too many driver bugs
 # in OpenGL implementation
-# try
-#     using GLAbstraction, GLWindow
-#     # we need at least OpenGL 4.1
-#     ctx = create_glcontext("test", resolution = (10, 10), major = 4, minor = 3)
-#     if ctx.handle != C_NULL
-#         info("opengl added as backend!")
-#         push!(supported_backends, :opengl)
-#     else
-#         error("Not a high enough version of OpenGL available. Try upgrading the video driver!")
-#     end
-# catch e
-#     info("OpenGL not added as backend: $e")
-# end
+try
+    using GLAbstraction, GLWindow
+    # we need at least OpenGL 4.1
+    ctx = GLWindow.create_glcontext("test", resolution = (10, 10), major = 3, minor = 3)
+    if ctx.handle != C_NULL
+        info("opengl added as backend!")
+        push!(supported_backends, :opengl)
+    else
+        error("Not a high enough version of OpenGL available. Try upgrading the video driver!")
+    end
+catch e
+    info("OpenGL not added as backend: $e")
+end
 
 
 supported_blas_libs = [:BLAS]
