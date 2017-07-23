00997025ad0e7799fe91727f9357e20245d738a7
SimonDanisch <sdanisch@gmail.com>
2017-07-23 17:28:34 +0200

    add examples

--- examples/pairwise.jl
+++ examples/pairwise.jl
@@ -11,16 +11,6 @@ function haversine{T <: Float32}(lat1::T, lon1::T, lat2::T, lon2::T, radius::T)
     return radius * c
 end
 
-function pairwise_dist4(i, lat, lon, rowresult, n)
-    for j in 1:n
-        @inbounds rowresult[i, j] = haversine(lat[i], lon[i], lat[j], lon[j] , 6372.8f0)
-    end
-end
-function pairwise_dist_cpu(lat, lon, rowresult, n)
-    for i = 1:n, j in 1:n
-        @inbounds rowresult[i, j] = haversine(lat[i], lon[i], lat[j], lon[j] , 6372.8f0)
-    end
-end
 
 using GPUArrays
 
@@ -36,3 +26,33 @@ abc = map(GPUArray, ABC);
 
 @time benchmark(abc...);
 @time pairwise_dist_cpu(ABC..., N);
+
+using GPUArrays
+CLBackend.init()
+function haversine_gpu(lat1, lon1, lat2, lon2, radius)
+    c1 = cospi(lat1 / 180.0f0)
+    c2 = cospi(lat2 / 180.0f0)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    d1 = sinpi(dlat / 360.0f0)
+    d2 = sinpi(dlon / 360.0f0)
+    t = d2 * d2 * c1 * c2
+    a = d1 * d1 + t
+    c = 2.0f0 * asin(min(1.0f0, sqrt(a)))
+    return radius * c
+end
+
+testdata = ntuple(i-> rand(Float32, 10^7), 4)
+gpu_testdata = map(GPUArray, testdata)
+result = similar(first(testdata))
+gpu_result = GPUArray(result)
+
+function bench(result, data)
+    result .= haversine_gpu.(data..., 4f0)
+    GPUArrays.synchronize(result)
+end
+
+bcpu = @elapsed bench(result, testdata)
+bgpu = @elapsed bench(gpu_result, gpu_testdata)
+@assert Array(gpu_result) ≈ result
+bcpu / bgpu
