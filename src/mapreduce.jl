b7b8801075d758d88b3783096acef54575ff546c
Ed Schmerling <ednerd@gmail.com>
2018-02-18 15:16:23 -0800

    Added all to mapreduce.jl, tests for any/all and rand/rand!

--- src/mapreduce.jl
+++ src/mapreduce.jl
@@ -1,9 +1,10 @@
-import Base: any, count, countnz
+import Base: any, all, count, countnz
 
 #############################
 # reduce
 # functions in base implemented with a direct loop need to be overloaded to use mapreduce
 any(pred, A::GPUArray) = Bool(mapreduce(pred, |, Int32(0), A))
+all(pred, A::GPUArray) = Bool(mapreduce(pred, &, Int32(1), A))
 count(pred, A::GPUArray) = Int(mapreduce(pred, +, UInt32(0), A))
 countnz(A::GPUArray) = Int(mapreduce(x-> x != 0, +, UInt32(0), A))
 countnz(A::GPUArray, dim) = Int(mapreducedim(x-> x != 0, +, UInt32(0), A, dim))
---
+++ src/testsuite/random.jl
@@ -0,0 +1,20 @@
+using GPUArrays
+using Base.Test, GPUArrays.TestSuite
+
+function run_random(Typ)
+    @testset "Random" begin
+        @testset "rand" begin  # uniform
+            for T in (Float32, Float64)
+                @test length(rand(Typ{T,1}, (4,))) == 4
+                @test length(rand(Typ, T, 4)) == 4
+                @test length(rand(Typ{T,2}, (4,5))) == 20
+                @test length(rand(Typ, T, 4, 5)) == 20
+                A = rand(Typ{T,2}, (2,2))
+                B = copy(A)
+                @test all(A .== B)
+                rand!(B)
+                @test !any(A .== B)
+            end
+        end
+    end
+end
