bd26574677d859bf26c8dbb0e1ad1525e32cf3c3
americast <sayan.sinha@iitkgp.ac.in>
2018-05-12 14:12:52 +0530

    WIP - tiling

--- src/matmul.jl
+++ src/matmul.jl
@@ -1,32 +1,66 @@
 import CUDAnative
 
-function matmul_kernel(state, A::AbstractArray{T}, B::AbstractArray{T}, out, Asize, Bsize, outSize) where T
-    ilin = linear_index(state)
-    idx = GPUArrays.gpu_ind2sub(outSize, ilin)
-    if (idx[1] > outSize[1] || idx[2] > outSize[2])
+function matmul_kernel(state, A::AbstractArray{T}, B::AbstractArray{T}, out, Asize, Bsize, outSize) where {T}
+    # Thread identifiers
+    TS = UInt32(2)
+    row = threadidx_x(state) # Local row ID (max: TS)
+    col = threadidx_y(state)
+    # col = row
+
+
+    groups_1 = blockidx_x(state)
+    groups_2 = blockidx_y(state)
+    # CUDAnative.@cuprintf("curr: %d\n", row)
+
+    globalRow = TS * (groups_1[1] - 1) + (row[1] - 1) + 1 # Row ID of C (0..M)
+    globalCol = TS * (groups_2[1] - 1) + (col[1] - 1) + 1 # Col ID of C (0..N)
+    if globalRow > (Asize[1] - TS) || globalCol > (Bsize[2] - TS)
         return
     end
-
-    # const int globalRow = get_global_id(0); // Row ID of C (0..M)
-    # const int globalCol = get_global_id(1); // Col ID of C (0..N)
+ 
+    # Local memory to fit a tile of TS*TS elements of A and B
+    Asub = zeros(Float32, TS * TS)
+    Bsub = zeros(Float32, TS * TS)
+ 
+    # Initialise the accumulation register
+    acc = Float32(0.0)
     
-    # // Compute a single element (loop over K)
-    acc = 0.0
-    for k in 1:Asize[2]
-        acc += A[(k - 1) * Asize[1] + (idx[1] - 1) + 1] * B[(idx[2] - 1) *Asize[2] + (k - 1) + 1];
+    # Loop over all tiles
+    numTiles = Asize[2]/TS
+    for t in UInt32(1):UInt32(numTiles)
+ 
+        # Load one tile of A and B into local memory
+        tiledRow = TS * (t - 1) + (row[1] - 1) + 1
+        tiledCol = TS * (t - 1) + (col[1] - 1) + 1
+        Asub[(col[1] - 1) * TS + (row[1] - 1) + 1] = A[(tiledCol - 1) * Asize[1] + (globalRow - 1) + 1]
+        Bsub[(col[1] - 1) * TS + (row[1] - 1) + 1] = B[(globalCol - 1) * Asize[2] + (tiledRow - 1) + 1]
+ 
+        # Synchronise to make sure the tile is loaded
+        synchronize_threads(state)
+ 
+        # Perform the computation for a single tile
+        for k in UInt32(1):UInt32(TS)
+            acc += Asub[(k - 1)*TS + (row[1] - 1 ) + 1] * Bsub[(col[1] - 1) * TS + (k - 1) + 1]
+        end
+ 
+        # Synchronise before loading the next tile
+        synchronize_threads(state)
     end
-    out[(idx[2] - 1) * Asize[1] + (idx[1] - 1) + 1] = acc
-    return
+ 
+    # Store the final result in C
+    out[(globalCol - 1) * Asize[1] + (globalRow - 1) + 1] = acc
+
+    return nothing
 end
 
 
-function matmul(a, b)
+function matmul(a::GPUArray{T, 2}, b::GPUArray{T, 2}) where T
     Asize = size(a)
     Bsize = size(b)
     out = similar(a, Asize[1], Bsize[2])
     outSize = size(out)
     Asize = UInt32.(Asize)
-    gpu_call(matmul_kernel, out, (a,b, out, UInt32.(Asize), UInt32.(Bsize), UInt32.(outSize)))
+    gpu_call(matmul_kernel, out, (a,b, out, UInt32.(Asize), UInt32.(Bsize), UInt32.(outSize)), (ceil.(UInt32, size(out) ./ (UInt32(2), UInt32(2))), (UInt32(2), UInt32(2))))
     out
 end
 
