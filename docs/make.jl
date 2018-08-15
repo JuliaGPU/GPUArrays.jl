5933058d1fb1a586bb72d92831ba86c5b3e5af5c
Valentin Churavy <t-vachur@microsoft.com>
2018-08-15 14:40:11 -0700

    fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs()
 deploydocs(
     repo   = "github.com/JuliaGPU/GPUArrays.jl.git",
     julia  = "0.7",
-    osname = "linux"
+    osname = "linux",
     # no need to build anything here, re-use output of `makedocs`
     deps   = nothing,
     make   = nothing,
