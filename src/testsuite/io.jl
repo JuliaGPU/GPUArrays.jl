f3db760e6358f5ea40d8ac4d3d93d15e154ef740
Tim Besard <tim.besard@gmail.com>
2018-08-16 14:45:00 +0200

    Fix i/o show tests.

--- src/testsuite/io.jl
+++ src/testsuite/io.jl
@@ -2,7 +2,7 @@ function test_io(AT)
     @testset "input/output" begin
         @testset "showing" begin
           io = IOBuffer()
-          A = AT([1])
+          A = AT(Int64[1])
 
           show(io, MIME("text/plain"), A)
           seekstart(io)
@@ -10,7 +10,9 @@ function test_io(AT)
 
           show(io, MIME("text/plain"), A')
           seekstart(io)
-          @test String(take!(io)) == "1×1 LinearAlgebra.Adjoint{Int64,$AT{Int64,1}}:\n 1"
+          msg = String(take!(io)) # the printing of Adjoint depends on global state
+          @test msg == "1×1 Adjoint{Int64,$AT{Int64,1}}:\n 1" ||
+                msg == "1×1 LinearAlgebra.Adjoint{Int64,$AT{Int64,1}}:\n 1"
         end
     end
 end
