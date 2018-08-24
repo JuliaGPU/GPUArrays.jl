adf2dadee692b1a8b60b85777a86cd2661a62190
Tim Besard <tim.besard@gmail.com>
2018-08-24 17:08:45 +0200

    Run CuArrays tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,32 @@
 using GPUArrays, Test
 using GPUArrays.TestSuite
 
+using Pkg
 
-@testset "Julia reference implementation:" begin
+@testset "JLArray" begin
     GPUArrays.test(JLArray)
+end
+
+function test_package(package)
+    mktempdir() do devdir
+        withenv("JULIA_PKG_DEVDIR" => devdir) do
+            # try to install from the same branch of GPUArrays
+            try
+                branch = chomp(read(`git -C $(@__DIR__) rev-parse --abbrev-ref HEAD`, String))
+                Pkg.add(PackageSpec(name=package, rev=String(branch)))
+                @info "Installed $package from $branch branch"
+            catch ex
+                @warn "Could not install $package from same branch as GPUArrays, trying master branch" exception=ex
+                Pkg.add(PackageSpec(name=package, rev="master"))
+            end
+
+            Pkg.test(package)
+        end
+    end
+end
+
+if haskey(ENV, "GITLAB_CI")
+    @testset "CuArray" begin
+        test_package("CuArrays")
+    end
 end
