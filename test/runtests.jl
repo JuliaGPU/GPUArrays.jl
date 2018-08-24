6990e6877ba31d35888e7d38e7a53d514960d15e
Valentin Churavy <t-vachur@microsoft.com>
2018-08-24 10:42:19 -0700

    try getting branch name from environment on Gitlab CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,15 @@ using Pkg
     GPUArrays.test(JLArray)
 end
 
-function test_package(package)
+function test_package(package, branch=nothing)
     mktempdir() do devdir
         withenv("JULIA_PKG_DEVDIR" => devdir) do
             # try to install from the same branch of GPUArrays
             try
-                branch = chomp(read(`git -C $(@__DIR__) rev-parse --abbrev-ref HEAD`, String))
+                if branch === nothing
+                    branch = chomp(read(`git -C $(@__DIR__) rev-parse --abbrev-ref HEAD`, String))
+                    branch == "HEAD" && error("in detached HEAD state")
+                end
                 Pkg.add(PackageSpec(name=package, rev=String(branch)))
                 @info "Installed $package from $branch branch"
             catch ex
@@ -26,7 +29,8 @@ function test_package(package)
 end
 
 if haskey(ENV, "GITLAB_CI")
+    branch = ENV["CI_COMMIT_REF_NAME"]
     @testset "CuArray" begin
-        test_package("CuArrays")
+        test_package("CuArrays", branch)
     end
 end
