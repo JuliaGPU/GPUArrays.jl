38ccea23326beb921dc34597c7508ced974f5033
Tim Besard <tim.besard@gmail.com>
2018-10-31 16:27:05 +0100

    Match branches of Adapt.jl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,30 +7,26 @@ using Pkg
     GPUArrays.test(JLArray)
 end
 
-function test_package(package, branch=nothing)
-    mktempdir() do devdir
-        withenv("JULIA_PKG_DEVDIR" => devdir) do
-            # try to install from the same branch of GPUArrays
-            try
-                if branch === nothing
-                    branch = chomp(read(`git -C $(@__DIR__) rev-parse --abbrev-ref HEAD`, String))
-                    branch == "HEAD" && error("in detached HEAD state")
-                end
-                Pkg.add(PackageSpec(name=package, rev=String(branch)))
-                @info "Installed $package from $branch branch"
-            catch ex
-                @warn "Could not install $package from same branch as GPUArrays, trying master branch" exception=ex
-                Pkg.add(PackageSpec(name=package, rev="master"))
-            end
+if haskey(ENV, "GITLAB_CI")
+    using Pkg
 
-            Pkg.test(package)
+    function match_package(package, branch)
+            try
+            Pkg.add(PackageSpec(name=package, rev=String(branch)))
+            @info "Installed $package from $branch branch"
+        catch ex
+            @warn "Could not install $package from $branch branch, trying master" exception=ex
+            Pkg.add(PackageSpec(name=package, rev="master"))
+            @info "Installed $package from master branch"
         end
     end
-end
 
-if haskey(ENV, "GITLAB_CI")
     branch = ENV["CI_COMMIT_REF_NAME"]
+    for package in ("Adapt", "CuArrays")
+        match_package(package, branch)
+    end
+
     @testset "CuArray" begin
-        test_package("CuArrays", branch)
+        Pkg.test("CuArrays")
     end
 end
