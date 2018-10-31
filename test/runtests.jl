087b9fc28832d3e25eb6189d64261babf84ae16f
Tim Besard <tim.besard@gmail.com>
2018-10-31 16:42:12 +0100

    Fix package matching on Travis and AppVeyor.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,19 @@
 # GPUArrays development often happens in lockstep with other packages, so try to match branches
-if haskey(ENV, "GITLAB_CI")
-    using Pkg
-
-    function match_package(package, branch)
-            try
-            Pkg.add(PackageSpec(name=package, rev=String(branch)))
-            @info "Installed $package from $branch branch"
-        catch ex
-            @warn "Could not install $package from $branch branch, trying master" exception=ex
-            Pkg.add(PackageSpec(name=package, rev="master"))
-            @info "Installed $package from master branch"
-        end
-    end
-
-    branch = ENV["CI_COMMIT_REF_NAME"]
-    for package in ("Adapt", "CuArrays")
-        match_package(package, branch)
+using Pkg
+function match_package(package, var)
+    try
+        branch = ENV[var]
+        Pkg.add(PackageSpec(name=package, rev=branch))
+        @info "Installed $package from branch $branch"
+    catch ex
+        @warn "Could not install $package from $branch branch, trying master" exception=ex
+        Pkg.add(PackageSpec(name=package, rev="master"))
+        @info "Installed $package from master"
     end
 end
+haskey(ENV, "TRAVIS")    && match_package("Adapt", "TRAVIS_PULL_REQUEST_BRANCH")
+haskey(ENV, "APPVEYOR")  && match_package("Adapt", "APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH")
+haskey(ENV, "GITLAB_CI") && match_package("Adapt", "CI_COMMIT_REF_NAME")
 
 using GPUArrays, Test
 
@@ -26,6 +22,7 @@ using GPUArrays, Test
 end
 
 if haskey(ENV, "GITLAB_CI")
+    match_package("CuArrays", "CI_COMMIT_REF_NAME")
     @testset "CuArray" begin
         Pkg.test("CuArrays")
     end
