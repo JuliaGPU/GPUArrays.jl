22122ffe7156b50d80c8c1a2922dece581f95c34
Tim Besard <tim.besard@gmail.com>
2018-12-11 09:26:18 +0100

    Adapt has been tagged.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,3 @@
-# development often happens in lockstep with other packages,
-# so check-out the master branch of those packages.
-using Pkg
-if haskey(ENV, "TRAVIS") || haskey(ENV, "APPVEYOR") || haskey(ENV, "GITLAB_CI")
-    Pkg.add(PackageSpec(name="Adapt", rev="master"))
-end
-
 using GPUArrays, Test
 
 @testset "JLArray" begin
