1a9e6d283e2fa013d807afa2cb4c7788f14fb0bb
Lucas C Wilcox <lucas@swirlee.com>
2019-03-18 16:21:51 -0500

    Make the threads and blocks heuristic nonzero
    Make sure the minimum number of threads and blocks returned by the
    heuristic is 1.  This allows the following code to work.
    
    ```julia
    using CuArrays
    a = Array{Float64}(undef, 0)
    fill!(CuArray(a), 0)
    ```

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -1,6 +1,6 @@
 function thread_blocks_heuristic(len::Integer)
     # TODO better threads default
-    threads = min(len, 256)
-    blocks = ceil(Int, len / threads)
+    threads = clamp(len, 1, 256)
+    blocks = max(ceil(Int, len / threads), 1)
     (blocks,), (threads,)
 end
