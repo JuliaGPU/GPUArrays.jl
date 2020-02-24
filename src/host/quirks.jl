b47b8c76ab157d8d3abe4b74d34f85d9dd5caa17
Tim Besard <tim.besard@gmail.com>
2020-02-24 13:57:37 +0100

    Drop support for pre-1.3.

--- src/host/quirks.jl
+++ src/host/quirks.jl
@@ -3,25 +3,23 @@
 # NOTE: without contextual dispatch, we can only redefine methods where a GPU-specific
 #       type occurs in the signature (or we'll get a "fatal precompilation failure" error)
 
-if VERSION >= v"1.3.0-alpha.107"
-    _bcs1(a::Integer, b::Integer) = a == 1 ? b : (b == 1 ? a : (a == b ? a : throw(DimensionMismatch("arrays could not be broadcast to a common size"))))
-    _bcs1(a::Integer, b) = a == 1 ? b : (first(b) == 1 && last(b) == a ? b : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
-    _bcs1(a, b::Integer) = _bcs1(b, a)
-    _bcs1(a, b) = Broadcast._bcsm(b, a) ? Broadcast.axistype(b, a) : (Broadcast._bcsm(a, b) ? Broadcast.axistype(a, b) : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
+_bcs1(a::Integer, b::Integer) = a == 1 ? b : (b == 1 ? a : (a == b ? a : throw(DimensionMismatch("arrays could not be broadcast to a common size"))))
+_bcs1(a::Integer, b) = a == 1 ? b : (first(b) == 1 && last(b) == a ? b : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
+_bcs1(a, b::Integer) = _bcs1(b, a)
+_bcs1(a, b) = Broadcast._bcsm(b, a) ? Broadcast.axistype(b, a) : (Broadcast._bcsm(a, b) ? Broadcast.axistype(a, b) : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
 
-    _bcs(::Tuple{}, ::Tuple{}) = ()
-    _bcs(::Tuple{}, newshape::Tuple) = (newshape[1], _bcs((), Base.tail(newshape))...)
-    _bcs(shape::Tuple, ::Tuple{}) = (shape[1], _bcs(Base.tail(shape), ())...)
-    function _bcs(shape::Tuple, newshape::Tuple)
-        return (_bcs1(shape[1], newshape[1]), _bcs(Base.tail(shape), Base.tail(newshape))...)
-    end
-
-    broadcast_shape(shape::Tuple) = shape
-    broadcast_shape(shape::Tuple, shape1::Tuple, shapes::Tuple...) = broadcast_shape(_bcs(shape, shape1), shapes...)
-
-    @inline combine_axes(A, B...) = broadcast_shape(axes(A), combine_axes(B...))
-    combine_axes(A) = axes(A)
-
-    Broadcast._axes(::Broadcasted{<:AbstractGPUArrayStyle}, axes::Tuple) = axes
-    @inline Broadcast._axes(bc::Broadcasted{<:AbstractGPUArrayStyle}, ::Nothing) = combine_axes(bc.args...)
+_bcs(::Tuple{}, ::Tuple{}) = ()
+_bcs(::Tuple{}, newshape::Tuple) = (newshape[1], _bcs((), Base.tail(newshape))...)
+_bcs(shape::Tuple, ::Tuple{}) = (shape[1], _bcs(Base.tail(shape), ())...)
+function _bcs(shape::Tuple, newshape::Tuple)
+    return (_bcs1(shape[1], newshape[1]), _bcs(Base.tail(shape), Base.tail(newshape))...)
 end
+
+broadcast_shape(shape::Tuple) = shape
+broadcast_shape(shape::Tuple, shape1::Tuple, shapes::Tuple...) = broadcast_shape(_bcs(shape, shape1), shapes...)
+
+@inline combine_axes(A, B...) = broadcast_shape(axes(A), combine_axes(B...))
+combine_axes(A) = axes(A)
+
+Broadcast._axes(::Broadcasted{<:AbstractGPUArrayStyle}, axes::Tuple) = axes
+@inline Broadcast._axes(bc::Broadcasted{<:AbstractGPUArrayStyle}, ::Nothing) = combine_axes(bc.args...)
