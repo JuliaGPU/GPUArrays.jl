c2ef523fa35d50e8bdebc10a65713cafa33bfc45
aterenin <aterenin@users.noreply.github.com>
2020-05-10 19:12:02 +0100

    Add inv for triangular matrices.

--- test/testsuite/linalg.jl
+++ test/testsuite/linalg.jl
@@ -46,6 +46,10 @@ function test_linalg(AT)
             end
         end
 
+        @testset "inv for triangular" for TR in (UpperTriangular, LowerTriangular, UnitUpperTriangular, UnitLowerTriangular)
+            @test compare(x -> inv(TR(x)), AT, rand(Float32, 32, 32))
+        end
+
         @testset "permutedims" begin
             @test compare(x -> permutedims(x, (2, 1)), AT, rand(Float32, 2, 3))
             @test compare(x -> permutedims(x, (2, 1, 3)), AT, rand(Float32, 4, 5, 6))
