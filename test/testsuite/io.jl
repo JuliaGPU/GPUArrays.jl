913229424870817f26c676414ff6c241ea91fb66
Tim Besard <tim.besard@gmail.com>
2020-09-15 14:50:09 +0200

    Fix I/O tests for Julia 1.6.

--- test/testsuite/io.jl
+++ test/testsuite/io.jl
@@ -1,34 +1,33 @@
 @testsuite "input output" AT->begin
+
+    # compact=false to avoid type aliases
+    replstr(x, kv::Pair...) = sprint((io,x) -> show(IOContext(io, :compact => false, :limit => true, :displaysize => (24, 80), kv...), MIME("text/plain"), x), x)
+    showstr(x, kv::Pair...) = sprint((io,x) -> show(IOContext(io, :limit => true, :displaysize => (24, 80), kv...), x), x)
+
     @testset "showing" begin
-        io = IOBuffer()
+        # vectors and non-vector arrays showing
+        # are handled differently in base/arrayshow.jl
         A = AT(Int64[1])
-        B = AT(Int64[1 2;3 4]) # vectors and non-vector arrays showing
-                               # are handled differently in base/arrayshow.jl
+        B = AT(Int64[1 2;3 4])
 
-        show(io, MIME("text/plain"), A)
-        seekstart(io)
-        @test occursin(Regex("^1-element $AT{Int64,1.*}:\n 1\$"), String(take!(io)))
+        msg = replstr(A)
+        @test occursin(Regex("^1-element $AT{Int64,\\s?1}:\n 1\$"), msg)
 
-        show(io, A)
-        seekstart(io)
-        msg = String(take!(io)) # result of e.g. `print` differs on 32bit and 64bit machines
+        # # result of e.g. `print` differs on 32bit and 64bit machines
         # due to different definition of `Int` type
         # print([1]) shows as [1] on 64bit but Int64[1] on 32bit
+        msg = showstr(A)
         @test msg == "[1]" || msg == "Int64[1]"
 
-        show(io, MIME("text/plain"), B)
-        seekstart(io)
-        @test occursin(Regex("^2×2 $AT{Int64,2.*}:\n 1  2\n 3  4\$"), String(take!(io)))
+        msg = replstr(B)
+        @test occursin(Regex("^2×2 $AT{Int64,\\s?2.*}:\n 1  2\n 3  4\$"), msg)
 
-        show(io, B)
-        seekstart(io)
-        msg = String(take!(io))
+        msg = showstr(B)
         @test msg == "[1 2; 3 4]" || msg == "Int64[1 2; 3 4]"
 
-        show(io, MIME("text/plain"), A')
-        seekstart(io)
-        msg = String(take!(io)) # the printing of Adjoint depends on global state
-        @test occursin(Regex("^1×1 Adjoint{Int64,$AT{Int64,1.*}}:\n 1\$"), msg) ||
-            occursin(Regex("^1×1 LinearAlgebra.Adjoint{Int64,$AT{Int64,1.*}}:\n 1\$"), msg)
+        # the printing of Adjoint depends on global state
+        msg = replstr(A')
+        @test occursin(Regex("^1×1 Adjoint{Int64,\\s?$AT{Int64,\\s?1}}:\n 1\$"), msg) ||
+            occursin(Regex("^1×1 LinearAlgebra.Adjoint{Int64,\\s?$AT{Int64,\\s?1}}:\n 1\$"), msg)
     end
 end
