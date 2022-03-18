e3d9e84f4057e0085202c21552ff3da50e558051
Alexius Wadell <awadell@gmail.com>
2022-03-18 17:21:18 -0400

    Dispatch threads over input elements
    Dispatching over output size results in 1 read + 1 write for each output
    vs. dispatching over input gives 1 read per input + 1 write per output.

--- src/host/base.jl
+++ src/host/base.jl
@@ -1,5 +1,4 @@
 # common Base functionality
-
 import Base: _RepeatInnerOuter
 
 function repeat_inner_kernel!(
@@ -8,13 +7,18 @@ function repeat_inner_kernel!(
     inner::NTuple{N, Int},
     out::AbstractArray{<:Any, N}
 ) where {N}
-    dest_inds = @cartesianidx(out).I
-    # Get the "stride" index in each dimension, where the size
-    # of the stride is given by `inner`. The stride-index then
-    # corresponds to the index of the repeated value in `xs`.
-    src_inds = ntuple(i -> (dest_inds[i] - 1) ÷ inner[i] + 1, N)
+    # Get single element from src
+    idx = @cartesianidx xs
+    val = xs[idx]
 
-    @inbounds out[dest_inds...] = xs[src_inds...]
+    # Loop over "repeat" indices of inner
+    for rdx in CartesianIndices(inner)
+        # Get destination CartesianIndex
+        odx = ntuple(N) do i
+            @inbounds (idx[i]-1) * inner[i] + rdx[i]
+        end
+        @inbounds out[CartesianIndex(odx)] = val
+    end
 
     return nothing
 end
@@ -22,8 +26,7 @@ end
 function repeat_inner(xs::AnyGPUArray, inner)
     out = similar(xs, eltype(xs), inner .* size(xs))
     any(==(0), size(out)) && return out # consistent with `Base.repeat`
-
-    gpu_call(repeat_inner_kernel!, xs, inner, out; elements=prod(size(out)))
+    gpu_call(repeat_inner_kernel!, xs, inner, out; elements=length(xs))
     return out
 end
 
@@ -34,11 +37,18 @@ function repeat_outer_kernel!(
     outer::NTuple{N},
     out::AbstractArray{<:Any, N}
 ) where {N}
-    dest_inds = @cartesianidx(out).I
-    # Outer is just wrapping around the edges of `xs`.
-    src_inds = ntuple(i -> (dest_inds[i] - 1) % xssize[i] + 1, N)
+    # Get index to input element
+    idx = @cartesianidx xs
+    val = xs[idx]
 
-    @inbounds out[dest_inds...] = xs[src_inds...]
+    # Loop over repeat indices, copying val to out
+    for rdx in CartesianIndices(outer)
+        # Get destination CartesianIndex
+        odx = ntuple(N) do i
+            @inbounds idx[i] + xssize[i] * (rdx[i] -1)
+        end
+        @inbounds out[CartesianIndex(odx)] = val
+    end
 
     return nothing
 end
@@ -46,8 +56,7 @@ end
 function repeat_outer(xs::AnyGPUArray, outer)
     out = similar(xs, eltype(xs), outer .* size(xs))
     any(==(0), size(out)) && return out # consistent with `Base.repeat`
-
-    gpu_call(repeat_outer_kernel!, xs, size(xs), outer, out; elements=prod(size(out)))
+    gpu_call(repeat_outer_kernel!, xs, size(xs), outer, out; elements=length(xs))
     return out
 end
 
