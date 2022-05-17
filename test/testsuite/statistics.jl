959240509c1013f7dbe796abf44b49c1b6bce172
Tim Besard <tim.besard@gmail.com>
2022-05-17 16:29:54 +0200

    Forward test kwargs to the comparison function, not the constructor.

--- test/testsuite/statistics.jl
+++ test/testsuite/statistics.jl
@@ -8,28 +8,28 @@ using Statistics
         @testset "std" begin
             @test compare(std, AT, rand(ET, 10))
             @test compare(std, AT, rand(ET, 10,1,2))
-            @test compare(std, AT, rand(ET, 10,1,2); corrected=true)
-            @test compare(std, AT, rand(ET, 10,1,2); dims=1)
+            @test compare(A->std(A; corrected=true), AT, rand(ET, 10,1,2))
+            @test compare(A->std(A; dims=1), AT, rand(ET, 10,1,2))
         end
 
         @testset "var" begin
             @test compare(var, AT, rand(ET, 10))
             @test compare(var, AT, rand(ET, 10,1,2))
-            @test compare(var, AT, rand(ET, 10,1,2); corrected=true)
-            @test compare(var, AT, rand(ET, 10,1,2); dims=1)
-            @test compare(var, AT, rand(ET, 10,1,2); dims=[1])
-            @test compare(var, AT, rand(ET, 10,1,2); dims=(1,))
-            @test compare(var, AT, rand(ET, 10,1,2); dims=[2,3])
-            @test compare(var, AT, rand(ET, 10,1,2); dims=(2,3))
+            @test compare(A->var(A; corrected=true), AT, rand(ET, 10,1,2))
+            @test compare(A->var(A; dims=1), AT, rand(ET, 10,1,2))
+            @test compare(A->var(A; dims=[1]), AT, rand(ET, 10,1,2))
+            @test compare(A->var(A; dims=(1,)), AT, rand(ET, 10,1,2))
+            @test compare(A->var(A; dims=[2,3]), AT, rand(ET, 10,1,2))
+            @test compare(A->var(A; dims=(2,3)), AT, rand(ET, 10,1,2))
         end
 
         @testset "mean" begin
             @test compare(mean, AT, rand(ET, 2, 2))
-            @test compare(mean, AT, rand(ET, 2, 2); dims=2)
-            @test compare(mean, AT, rand(ET, 2, 2, 2); dims=[1,3])
-            @test compare(x->mean(sin, x), AT, rand(ET, 2,2))
-            @test compare(x->mean(sin, x; dims=2), AT, rand(ET, 2,2))
-            @test compare(x->mean(sin, x; dims=[1,3]), AT, rand(ET, 2,2,2))
+            @test compare(A->mean(A; dims=2), AT, rand(ET, 2, 2))
+            @test compare(A->mean(A; dims=[1,3]), AT, rand(ET, 2, 2, 2))
+            @test compare(A->mean(sin, A), AT, rand(ET, 2,2))
+            @test compare(A->mean(sin, A; dims=2), AT, rand(ET, 2,2))
+            @test compare(A->mean(sin, A; dims=[1,3]), AT, rand(ET, 2,2,2))
         end
     end
 
@@ -42,7 +42,7 @@ using Statistics
             s = 100
             @test compare(cov, AT, rand(ET, s))
             @test compare(cov, AT, rand(ET, s, 2))
-            @test compare(cov, AT, rand(ET, s, 2); dims=2)
+            @test compare(A->cov(A; dims=2), AT, rand(ET, s, 2))
             if ET <: Real
                 @test compare(cov, AT, rand(ET(1):ET(100), s))
             end
@@ -52,7 +52,7 @@ using Statistics
             s = 100
             @test compare(cor, AT, rand(ET, s))
             @test compare(cor, AT, rand(ET, s, 2))
-            @test compare(cor, AT, rand(ET, s, 2); dims=2)
+            @test compare(A->cor(A; dims=2), AT, rand(ET, s, 2))
             if ET <: Real
                 @test compare(cor, AT, rand(ET(1):ET(100), s))
             end
