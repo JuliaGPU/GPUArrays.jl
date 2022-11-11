e0a6fb522a3cda8a71e17c0633fe65062d8584ac
Simone Carlo Surace <51025924+simsurace@users.noreply.github.com>
2022-11-11 20:42:54 +0100

    Simplify prevention mechanism

--- test/testsuite/linalg.jl
+++ test/testsuite/linalg.jl
@@ -126,12 +126,8 @@
 
         @testset "cholesky + Diagonal" begin
             n = 128
-            
-            # To prevent failures due to random numbers being zero
-            d = AT(zeros(Float32, n))
-            while any(iszero, d)
-                d = AT(rand(Float32, n))
-            end
+            # Add one in order prevent failures due to random numbers being zero
+            d = AT(zeros(Float32, n) .+ one(Float32))
             D = Diagonal(d)
             F = collect(D)
             @test collect(cholesky(D).U) ≈ collect(cholesky(F).U)
