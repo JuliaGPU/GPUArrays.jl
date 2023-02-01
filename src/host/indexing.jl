78bdda3333cdee8338a1b8bab40b8c35302689c9
Tim Besard <tim.besard@gmail.com>
2023-02-01 15:54:56 +0100

    find: Avoid redefining functions on 1.6. (#449)

--- src/host/indexing.jl
+++ src/host/indexing.jl
@@ -151,7 +151,7 @@ function findminmax(binop, xs::AnyGPUArray; init, dims)
 
     @static if VERSION < v"1.7.0-DEV.119"
     # before JuliaLang/julia#35316, isless/isgreated did not order NaNs last
-    function reduction(t1, t2)
+    function reduction(t1::Tuple{<:AbstractFloat,<:Any}, t2::Tuple{<:AbstractFloat,<:Any})
         (x, i), (y, j) = t1, t2
 
         isnan(x) && return t1
