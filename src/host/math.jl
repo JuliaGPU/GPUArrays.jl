6fc6c7c4e29704fd7c17263fea7c89c75674bbbc
Tim Besard <tim.besard@gmail.com>
2023-03-20 09:32:36 +0100

    Revert "Avoid cartesian iteration where possible. (#454)"
    This reverts commit 36661e3533d4d96fd11ca1fa644cd143988d4605.

--- src/host/math.jl
+++ src/host/math.jl
@@ -2,7 +2,7 @@
 
 function Base.clamp!(A::AnyGPUArray, low, high)
     gpu_call(A, low, high) do ctx, A, low, high
-        I = @linearidx A
+        I = @cartesianidx A
         A[I] = clamp(A[I], low, high)
         return
     end
