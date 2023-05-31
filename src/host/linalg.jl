e50f24a6a5d820d83cf30693e8d6fba6489f64d8
Daniel Karrasch <Daniel.Karrasch@posteo.de>
2023-05-31 20:49:21 +0200

    update

--- src/host/linalg.jl
+++ src/host/linalg.jl
@@ -1,5 +1,29 @@
 # integration with LinearAlgebra stdlib
 
+using LinearAlgebra: MulAddMul
+
+if isdefined(LinearAlgebra, :wrap) # i.e., VERSION >= v"1.10.0-DEV.1365"
+    using LinearAlgebra: wrap
+else
+    function wrap(A::AbstractVecOrMat, tA::AbstractChar)
+        if tA == 'N'
+            return A
+        elseif tA == 'T'
+            return transpose(A)
+        elseif tA == 'C'
+            return adjoint(A)
+        elseif tA == 'H'
+            return Hermitian(A, :U)
+        elseif tA == 'h'
+            return Hermitian(A, :L)
+        elseif tA == 'S'
+            return Symmetric(A, :U)
+        else # tA == 's'
+            return Symmetric(A, :L)
+        end
+    end
+end
+
 ## transpose and adjoint
 
 function LinearAlgebra.transpose!(B::AbstractGPUVector, A::AbstractGPUMatrix)
@@ -319,28 +343,46 @@ function generic_matmatmul!(C::AbstractArray{R}, A::AbstractArray{T}, B::Abstrac
     C
 end
 
-using LinearAlgebra: MulAddMul
-
-function LinearAlgebra.gemv!(C::AbstractGPUVector, tA::AbstractChar, A::AbstractGPUMatrix, B::AbstractGPUVector, a::Number, b::Number)
-    transA = tA == 'N' ? identity : tA == 'T' ? transpose : adjoint
-    generic_matmatmul!(C, transA(A), B, a, b)
-end
 function LinearAlgebra.generic_matvecmul!(C::AbstractGPUVector, tA::AbstractChar, A::AbstractGPUMatrix, B::AbstractGPUVector, _add::MulAddMul = MulAddMul())
-    transA = tA == 'N' ? identity : tA == 'T' ? transpose : adjoint
-    generic_matmatmul!(C, transA(A), B, _add.alpha, _add.beta)
-end
-# disambiguation
-function LinearAlgebra.gemv!(C::AbstractGPUVector{T}, tA::AbstractChar, A::AbstractGPUMatrix{T}, B::AbstractGPUVector{T}, a::Number, b::Number) where {T<:LinearAlgebra.BlasFloat}
-    transA = tA == 'N' ? identity : tA == 'T' ? transpose : adjoint
-    generic_matmatmul!(C, transA(A), B, a, b)
+    generic_matmatmul!(C, wrap(A, tA), B, _add.alpha, _add.beta)
 end
 
 function LinearAlgebra.generic_matmatmul!(C::AbstractGPUVecOrMat, tA, tB, A::AbstractGPUVecOrMat, B::AbstractGPUVecOrMat, _add::MulAddMul=MulAddMul())
-    transA = tA == 'N' ? identity : tA == 'T' ? transpose : adjoint
-    transB = tB == 'N' ? identity : tB == 'T' ? transpose : adjoint
-    generic_matmatmul!(C, transA(A), transB(B), _add.alpha, _add.beta)
+    generic_matmatmul!(C, wrap(A, tA), wrap(B, tB), _add.alpha, _add.beta)
 end
 
+if VERSION < v"1.10.0-DEV.1365"
+# catch other functions that are called by LinearAlgebra's mul!
+function LinearAlgebra.gemv!(C::AbstractGPUVector, tA::AbstractChar, A::AbstractGPUMatrix, B::AbstractGPUVector, a::Number, b::Number)
+    generic_matmatmul!(C, wrap(A, tA), B, a, b)
+end
+# disambiguation
+function LinearAlgebra.gemv!(C::AbstractGPUVector{T}, tA::AbstractChar, A::AbstractGPUMatrix{T}, B::AbstractGPUVector{T}, a::Number, b::Number) where {T<:LinearAlgebra.BlasFloat}
+    generic_matmatmul!(C, wrap(A, tA), B, a, b)
+end
+
+LinearAlgebra.gemm_wrapper!(C::AbstractGPUVecOrMat, tA, tB, A::AbstractGPUVecOrMat, B::AbstractGPUVecOrMat, _add::MulAddMul) =
+    LinearAlgebra.generic_matmatmul!(C, tA, tB, A, B, _add)
+# disambiguation
+LinearAlgebra.gemm_wrapper!(C::AbstractGPUVecOrMat{T}, tA, tB, A::AbstractGPUVecOrMat{T}, B::AbstractGPUVecOrMat{T}, _add::MulAddMul) where {T<:LinearAlgebra.BlasFloat} =
+    LinearAlgebra.generic_matmatmul!(C, tA, tB, A, B, _add)
+
+function LinearAlgebra.syrk_wrapper!(C::AbstractGPUMatrix, tA::AbstractChar, A::AbstractGPUVecOrMat, _add::MulAddMul = MulAddMul())
+    if tA == 'T'
+        LinearAlgebra.generic_matmatmul!(C, 'T', 'N', A, A, _add)
+    else # tA == 'N'
+        LinearAlgebra.generic_matmatmul!(C, 'N', 'T', A, A, _add)
+    end
+end
+function LinearAlgebra.herk_wrapper!(C::AbstractGPUMatrix, tA::AbstractChar, A::AbstractGPUVecOrMat, _add::MulAddMul = MulAddMul())
+    if tA == 'C'
+        LinearAlgebra.generic_matmatmul!(C, 'C', 'N', A, A, _add)
+    else # tA == 'N'
+        LinearAlgebra.generic_matmatmul!(C, 'N', 'C', A, A, _add)
+    end
+end
+end # VERSION
+    
 function generic_rmul!(X::AbstractArray, s::Number)
     gpu_call(X, s; name="rmul!") do ctx, X, s
         i = @linearidx X
