70a61ea5a43fe629fbf8a2497cd4568901c91cd3
Miles Cranmer <miles.cranmer@gmail.com>
2024-01-14 20:52:16 +0000

    Fix docstring

--- src/device/abstractarray.jl
+++ src/device/abstractarray.jl
@@ -9,8 +9,8 @@ export AbstractDeviceArray
     AbstractDeviceArray{T, N} <: DenseArray{T, N}
 
 Supertype for `N`-dimensional GPU arrays (or array-like types) with elements of type `T`.
-Instances of this type are expected to live on the host, see [`AbstractGPUArray`](@ref)
-for device-side objects.
+Instances of this type are expected to live on the device, see [`AbstractGPUArray`](@ref)
+for host-side objects.
 """
 abstract type AbstractDeviceArray{T, N} <: DenseArray{T, N} end
 
