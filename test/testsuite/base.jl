d828fb1a8a12e52c50f3bb0ef8b9b01bb9da7c82
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-05-18 12:24:29 -0300

    Port `reverse` from CUDA

--- test/testsuite/base.jl
+++ test/testsuite/base.jl
@@ -381,6 +381,51 @@ end
       gA = reshape(AT(A),4)
     end
 
+    @testset "reverse" begin
+      # 1-d out-of-place
+      @test compare(x->reverse(x), AT, rand(Float32, 1000))
+      @test compare(x->reverse(x, 10), AT, rand(Float32, 1000))
+      @test compare(x->reverse(x, 10, 90), AT, rand(Float32, 1000))
+
+      # 1-d in-place
+      @test compare(x->reverse!(x), AT, rand(Float32, 1000))
+      @test compare(x->reverse!(x, 10), AT, rand(Float32, 1000))
+      @test compare(x->reverse!(x, 10, 90), AT, rand(Float32, 1000))
+
+      # n-d out-of-place
+      for shape in ([1, 2, 4, 3], [4, 2], [5], [2^5, 2^5, 2^5]),
+          dim in 1:length(shape)
+        @test compare(x->reverse(x; dims=dim), AT, rand(Float32, shape...))
+
+        cpu = rand(Float32, shape...)
+        gpu = AT(cpu)
+        reverse!(gpu; dims=dim)
+        @test Array(gpu) == reverse(cpu; dims=dim)
+      end
+
+      # supports multidimensional reverse
+      for shape in ([1, 2, 4, 3], [2^5, 2^5, 2^5]),
+          dim in ((1,2),(2,3),(1,3),:)
+        @test compare(x->reverse(x; dims=dim), AT, rand(Float32, shape...))
+
+        cpu = rand(Float32, shape...)
+        gpu = AT(cpu)
+        reverse!(gpu; dims=dim)
+        @test Array(gpu) == reverse(cpu; dims=dim)
+      end
+
+      # wrapped array
+      @test compare(x->reverse(x), AT, reshape(rand(Float32, 2,2), 4))
+
+      # error throwing
+      cpu = rand(Float32, 1,2,3,4)
+      gpu = AT(cpu)
+      @test_throws ArgumentError reverse!(gpu, dims=5)
+      @test_throws ArgumentError reverse!(gpu, dims=0)
+      @test_throws ArgumentError reverse(gpu, dims=5)
+      @test_throws ArgumentError reverse(gpu, dims=0)
+  end
+
     @testset "reinterpret" begin
       A = Int32[-1,-2,-3]
       dA = AT(A)
