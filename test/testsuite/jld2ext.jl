268e22622e55c9c1de8885b9e822964c9083b016
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-06-16 17:52:14 -0300

    Minor cleanup

--- test/testsuite/jld2ext.jl
+++ test/testsuite/jld2ext.jl
@@ -2,15 +2,15 @@ using JLD2
 using Test
 
 @testsuite "ext/jld2" (AT, eltypes) -> begin
-    for ET in eltypes
-        @testset "$ET" begin
-            # Test with different array sizes and dimensions
-            for dims in ((2,), (2, 2), (2, 2, 2))
-                # Create a random array
-                x = AT(rand(ET, dims...))
+    mktempdir() do dir
+        for ET in eltypes
+            @testset "$ET" begin
+                # Test with different array sizes and dimensions
+                for dims in ((2,), (2, 2), (2, 2, 2))
+                    # Create a random array
+                    x = AT(rand(ET, dims...))
 
-                # Save to a temporary file
-                mktempdir() do dir
+                    # Save to a temporary file
                     file = joinpath(dir, "test.jld2")
 
                     # Save and load
