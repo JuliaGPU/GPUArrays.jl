8daec657a9c58aaf5d38131bd2901f0bb1b57d96
Avik Pal <avikpal@mit.edu>
2025-09-01 14:28:07 -0400

    chore: fix runic formatting

--- src/host/random.jl
+++ src/host/random.jl
@@ -97,7 +97,7 @@ function Random.randn!(rng::RNG, A::AnyGPUArray{T}) where T <: Number
     @kernel function randn!(a, randstates)
         i = @index(Global, Linear)
         idx = 2*(i - 1) + 1
-        threadidx = (i-1)%length(randstates) + 1
+        threadidx = (i - 1) % length(randstates) + 1
         U1 = gpu_rand(T, threadidx, randstates)
         U2 = gpu_rand(T, threadidx, randstates)
         Z0 = sqrt(T(-2.0)*log(U1))*cos(T(2pi)*U2)
