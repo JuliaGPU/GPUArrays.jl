6ee51c97d88eed43c6c3a5ada676171c76dce07c
Matt Fishman <mtfishman@users.noreply.github.com>
2025-10-09 18:15:06 -0400

    Format

--- test/testsuite/linalg.jl
+++ test/testsuite/linalg.jl
@@ -17,7 +17,6 @@
     @testset "tr" begin
         @test compare(tr, AT, rand(Float32, 32, 32))
     end
-    
     @testset "permutedims" begin
         @test compare(x -> permutedims(x, (2, 1)), AT, rand(Float32, 2, 3))
         @test compare(x -> permutedims(x, (2, 1, 3)), AT, rand(Float32, 4, 5, 6))
