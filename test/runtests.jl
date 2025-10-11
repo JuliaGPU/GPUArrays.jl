6c9dc96140fa46f5763a20b083304597d879f5b4
Valentin Churavy <v.churavy@gmail.com>
2025-10-11 14:48:21 +0200

    Switch to symbols instead of using the types directly

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using ParallelTestRunner
-using JLArrays
 
 include("testsuite.jl")
 
@@ -9,14 +8,14 @@ const init_code = quote
     include("testsuite.jl")
 
     # Disable Float16-related tests until JuliaGPU/KernelAbstractions#600 is resolved
-    @static if isdefined(JLArrays.KernelAbstractions, :POCL)
+    if isdefined(JLArrays.KernelAbstractions, :POCL)
         TestSuite.supported_eltypes(::Type{<:JLArray}) =
             setdiff(TestSuite.supported_eltypes(), [Float16, ComplexF16])
     end
 end
 
 custom_tests = Dict{String, Expr}()
-for AT in (JLArray, Array), name in keys(TestSuite.tests)
+for AT in (:JLArray, :Array), name in keys(TestSuite.tests)
     custom_tests["$(AT)/$name"] = :(TestSuite.tests[$name]($AT))
 end
 
