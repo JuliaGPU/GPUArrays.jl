c9b19c911e6a797e3fd48c2d81f21ecf4c0d76e6
Github action runner <git@github.com>
2025-11-26 13:57:56 -0800

    Runiced

--- test/testsuite/statistics.jl
+++ test/testsuite/statistics.jl
@@ -9,29 +9,29 @@ using Statistics
 
         @testset "std" begin
             @test compare(std, AT, rand(range, 10))
-            @test compare(std, AT, rand(range, 10,1,2))
-            @test compare(A->std(A; corrected=true), AT, rand(range, 10,1,2))
-            @test compare(A->std(A; dims=1), AT, rand(range, 10,1,2))
+            @test compare(std, AT, rand(range, 10, 1, 2))
+            @test compare(A -> std(A; corrected = true), AT, rand(range, 10, 1, 2))
+            @test compare(A -> std(A; dims = 1), AT, rand(range, 10, 1, 2))
         end
 
         @testset "var" begin
             @test compare(var, AT, rand(range, 10))
-            @test compare(var, AT, rand(range, 10,1,2))
-            @test compare(A->var(A; corrected=true), AT, rand(range, 10,1,2))
-            @test compare(A->var(A; dims=1), AT, rand(range, 10,1,2))
-            @test compare(A->var(A; dims=[1]), AT, rand(range, 10,1,2))
-            @test compare(A->var(A; dims=(1,)), AT, rand(range, 10,1,2))
-            @test compare(A->var(A; dims=[2,3]), AT, rand(range, 10,1,2))
-            @test compare(A->var(A; dims=(2,3)), AT, rand(range, 10,1,2))
+            @test compare(var, AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; corrected = true), AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; dims = 1), AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; dims = [1]), AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; dims = (1,)), AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; dims = [2, 3]), AT, rand(range, 10, 1, 2))
+            @test compare(A -> var(A; dims = (2, 3)), AT, rand(range, 10, 1, 2))
         end
 
         @testset "mean" begin
             @test compare(mean, AT, rand(range, 2, 2))
-            @test compare(A->mean(A; dims=2), AT, rand(range, 2, 2))
-            @test compare(A->mean(A; dims=[1,3]), AT, rand(range, 2, 2, 2))
-            @test compare(A->mean(sin, A), AT, rand(range, 2,2))
-            @test compare(A->mean(sin, A; dims=2), AT, rand(range, 2,2))
-            @test compare(A->mean(sin, A; dims=[1,3]), AT, rand(range, 2,2,2))
+            @test compare(A -> mean(A; dims = 2), AT, rand(range, 2, 2))
+            @test compare(A -> mean(A; dims = [1, 3]), AT, rand(range, 2, 2, 2))
+            @test compare(A -> mean(sin, A), AT, rand(range, 2, 2))
+            @test compare(A -> mean(sin, A; dims = 2), AT, rand(range, 2, 2))
+            @test compare(A -> mean(sin, A; dims = [1, 3]), AT, rand(range, 2, 2, 2))
         end
     end
 
@@ -46,14 +46,14 @@ using Statistics
             s = 100
             @test compare(cov, AT, rand(range, s))
             @test compare(cov, AT, rand(range, s, 2))
-            @test compare(A->cov(A; dims=2), AT, rand(range, s, 2))
+            @test compare(A -> cov(A; dims = 2), AT, rand(range, s, 2))
         end
 
         @testset "cor" begin
             s = 100
-            @test compare(cor, AT, rand(range, s)) nans=true
-            @test compare(cor, AT, rand(range, s, 2)) nans=true
-            @test compare(A->cor(A; dims=2), AT, rand(range, s, 2)) nans=true
+            @test compare(cor, AT, rand(range, s)) nans = true
+            @test compare(cor, AT, rand(range, s, 2)) nans = true
+            @test compare(A -> cor(A; dims = 2), AT, rand(range, s, 2)) nans = true
         end
     end
 end
