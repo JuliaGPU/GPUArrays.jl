e4e19dd87efd3eb24a61d10dd616fe10e6f598e8
Erik Schnetter <schnetter@gmail.com>
2026-01-31 15:16:26 -0500

    copytri! has a diag argument for Julia <1.13

--- src/host/linalg.jl
+++ src/host/linalg.jl
@@ -107,102 +107,48 @@ end
 
 ## copy upper triangle to lower and vice versa
 
-# `v"1.13-"` is the smallest possible 1.13 version number. It comes
-# before all alpha, beta, and rc prereleases. (A plain `v"1.13"` would
-# come after all prereleases.)
-if VERSION >= v"1.13-"
-
-    function LinearAlgebra.copytri!(A::AbstractGPUMatrix, uplo::AbstractChar, conjugate::Bool = false, diag::Bool = false)
-        n = LinearAlgebra.checksquare(A)
-        if uplo == 'U' && conjugate
-            @kernel function U_conj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j + diag > i
-                    @inbounds _A[j,i] = conj(_A[i,j])
-              end
-            end
-            U_conj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'U' && !conjugate
-            @kernel function U_noconj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j + diag > i
-                    @inbounds _A[j,i] = _A[i,j]
-              end
-            end
-            U_noconj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'L' && conjugate
-            @kernel function L_conj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j + diag > i
-                    @inbounds _A[i,j] = conj(_A[j,i])
-                end
-            end
-            L_conj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'L' && !conjugate
-            @kernel function L_noconj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j + diag > i
-                    @inbounds _A[i,j] = _A[j,i]
-                end
-            end
-            L_noconj!(get_backend(A))(A; ndrange = size(A))
-        else
-            throw(ArgumentError("uplo argument must be 'U' (upper) or 'L' (lower), got $uplo"))
+function LinearAlgebra.copytri!(A::AbstractGPUMatrix, uplo::AbstractChar, conjugate::Bool = false, diag::Bool = false)
+    n = LinearAlgebra.checksquare(A)
+    if uplo == 'U' && conjugate
+        @kernel function U_conj!(_A)
+            I = @index(Global, Cartesian)
+            i, j = Tuple(I)
+            if j + diag > i
+                @inbounds _A[j,i] = conj(_A[i,j])
+          end
         end
-        A
-    end
-
-else
-
-    # For backward compatibility with Julia <1.13 where there is no `diag` argument
-    function LinearAlgebra.copytri!(A::AbstractGPUMatrix, uplo::AbstractChar, conjugate::Bool = false)
-        n = LinearAlgebra.checksquare(A)
-        if uplo == 'U' && conjugate
-            @kernel function U_conj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j > i
-                    @inbounds _A[j,i] = conj(_A[i,j])
-              end
-            end
-            U_conj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'U' && !conjugate
-            @kernel function U_noconj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j > i
-                    @inbounds _A[j,i] = _A[i,j]
-              end
-            end
-            U_noconj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'L' && conjugate
-            @kernel function L_conj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j > i
-                    @inbounds _A[i,j] = conj(_A[j,i])
-                end
-            end
-            L_conj!(get_backend(A))(A; ndrange = size(A))
-        elseif uplo == 'L' && !conjugate
-            @kernel function L_noconj!(_A)
-                I = @index(Global, Cartesian)
-                i, j = Tuple(I)
-                if j > i
-                    @inbounds _A[i,j] = _A[j,i]
-                end
-            end
-            L_noconj!(get_backend(A))(A; ndrange = size(A))
-        else
-            throw(ArgumentError("uplo argument must be 'U' (upper) or 'L' (lower), got $uplo"))
+        U_conj!(get_backend(A))(A; ndrange = size(A))
+    elseif uplo == 'U' && !conjugate
+        @kernel function U_noconj!(_A)
+            I = @index(Global, Cartesian)
+            i, j = Tuple(I)
+            if j + diag > i
+                @inbounds _A[j,i] = _A[i,j]
+          end
         end
-        A
+        U_noconj!(get_backend(A))(A; ndrange = size(A))
+    elseif uplo == 'L' && conjugate
+        @kernel function L_conj!(_A)
+            I = @index(Global, Cartesian)
+            i, j = Tuple(I)
+            if j + diag > i
+                @inbounds _A[i,j] = conj(_A[j,i])
+            end
+        end
+        L_conj!(get_backend(A))(A; ndrange = size(A))
+    elseif uplo == 'L' && !conjugate
+        @kernel function L_noconj!(_A)
+            I = @index(Global, Cartesian)
+            i, j = Tuple(I)
+            if j + diag > i
+                @inbounds _A[i,j] = _A[j,i]
+            end
+        end
+        L_noconj!(get_backend(A))(A; ndrange = size(A))
+    else
+        throw(ArgumentError("uplo argument must be 'U' (upper) or 'L' (lower), got $uplo"))
     end
-
+    A
 end
 
 ## copy a triangular part of a matrix to another matrix
--- test/testsuite/linalg.jl
+++ test/testsuite/linalg.jl
@@ -75,32 +75,15 @@
 
     @testset "triangular" begin
         @testset "copytri!" begin
-            # `v"1.13-"` is the smallest possible 1.13 version number.
-            # It comes before all alpha, beta, and rc prereleases. (A
-            # plain `v"1.13"` would come after all prereleases.)
-            if VERSION >= v"1.13-"
-                @testset for eltya in (Float32, Float64, ComplexF32, ComplexF64), uplo in ('U', 'L'), conjugate in (true, false), diag in (true, false)
-                    if !(eltya in eltypes)
-                        continue
-                    end
-                    n = 128
-                    areal = randn(n,n)/2
-                    aimg  = randn(n,n)/2
-                    a = convert(Matrix{eltya}, eltya <: Complex ? complex.(areal, aimg) : areal)
-                    @test compare(x -> LinearAlgebra.copytri!(x, uplo, conjugate, diag), AT, a)
+            @testset for eltya in (Float32, Float64, ComplexF32, ComplexF64), uplo in ('U', 'L'), conjugate in (true, false), diag in (true, false)
+                if !(eltya in eltypes)
+                    continue
                 end
-            else
-                # Julia <=1.12 does not have a `diag` argument
-                @testset for eltya in (Float32, Float64, ComplexF32, ComplexF64), uplo in ('U', 'L'), conjugate in (true, false)
-                    if !(eltya in eltypes)
-                        continue
-                    end
-                    n = 128
-                    areal = randn(n,n)/2
-                    aimg  = randn(n,n)/2
-                    a = convert(Matrix{eltya}, eltya <: Complex ? complex.(areal, aimg) : areal)
-                    @test compare(x -> LinearAlgebra.copytri!(x, uplo, conjugate), AT, a)
-                end
+                n = 128
+                areal = randn(n,n)/2
+                aimg  = randn(n,n)/2
+                a = convert(Matrix{eltya}, eltya <: Complex ? complex.(areal, aimg) : areal)
+                @test compare(x -> LinearAlgebra.copytri!(x, uplo, conjugate, diag), AT, a)
             end
         end
 
