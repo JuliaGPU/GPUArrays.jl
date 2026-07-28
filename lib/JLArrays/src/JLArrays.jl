65eb84780624fc7a186bd12d0de086adc8a20d79
Simeon David Schaub <simeon@schaub.rocks>
2026-07-28 14:36:27 +0200

    [JLArrays] allow `mapreducedim!` into wrapper
    Previously, e.g. `sum!(transpose(v), M)` would error

--- lib/JLArrays/src/JLArrays.jl
+++ lib/JLArrays/src/JLArrays.jl
@@ -555,12 +555,15 @@ else
     end
 end
 
+struct ArrayNoCopy end
+Adapt.adapt_storage(::ArrayNoCopy, x::JLArray) = typed_data(x)
+
 function GPUArrays.mapreducedim!(f, op, R::AnyJLArray, A::Union{AbstractArray,Broadcast.Broadcasted};
                                  init=nothing)
     if init !== nothing
         fill!(R, init)
     end
-    @allowscalar Base.reducedim!(op, typed_data(R), map(f, A))
+    @allowscalar Base.reducedim!(op, adapt(ArrayNoCopy(), R), map(f, A))
     R
 end
 
