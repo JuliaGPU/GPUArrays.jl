7907599c865801ea717f5a3186a37e9e44c3151a
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-09-24 10:35:27 -0300

    [NFC] Better organize math tests

--- test/testsuite/math.jl
+++ test/testsuite/math.jl
@@ -1,23 +1,19 @@
 @testsuite "math" (AT, eltypes)->begin
-    # clamp
-    for ET in filter(!iscomplextype, eltypes)
-        T = AT{ET}
-        @testset "$ET" begin
+    @testset "$ET" for ET in eltypes
+        @testset "power" begin
+            for p in [0,1,2,5]
+                @test compare(x->x^p, AT, rand(ET, 2,2))
+            end
+        end
+
+        iscomplextype(ET) && continue
+        @testset "clamp!" begin
             range = ET <: Integer ? (ET(-2):ET(2)) : ET
             low = ET(-1)
             high = ET(1)
-            @testset "clamp!" begin
-                for N in (2, 10)
-                    @test compare(x -> clamp!(x, low, high), AT, rand(range, N, N))
-                end
+            for N in (2, 10)
+                @test compare(x -> clamp!(x, low, high), AT, rand(range, N, N))
             end
-        end
-    end
-
-    # power
-    for ET in eltypes
-        for p in [0,1,2,5]
-            @test compare(x->x^p, AT, rand(ET, 2,2))
         end
     end
 end
